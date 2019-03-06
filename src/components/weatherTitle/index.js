9d133104b88f8eb1a3293e618fa491b17e64d85e
bg2345 <bgardynski@gmail.com>
2019-03-06 11:30:48 -0500

    Rendering routes and entire weather component. TODO: setup form and api call

---
+++ src/components/weatherTitle/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+class WeatherTitle extends Component {
+  render() {
+    return (
+      <div className="weatherTitle">
+        <h1>Check the Weather!</h1>
+      </div>
+
+    );
+  }
+}
+
+export default WeatherTitle;
---
+++ src/views/sample/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+class Sample extends Component {
+  render() {
+    return (
+      <h1>This is the weather page</h1>
+
+    );
+  }
+}
+
+export default Sample;
